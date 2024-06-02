7049b90bb26eebb26a6246ca6c01bc794c51f79f
Mikaila Brown <mbr2201@wgu.edu>
2024-06-02 07:58:20 -0400

    update allowed host

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["content-metrics-tracker.herokuapp.com"]
+ALLOWED_HOSTS = ["content-metrics-tracker-adb5d4e462a6.herokuapp.com"]
 
 
 # Application definition
